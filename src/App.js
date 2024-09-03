14bfb47d29be1a314cdebc3bc82739c3f0eb88e0
Artem <strikerlutsk@gmai.comuser.email>
2024-09-03 19:55:55 +0200

    Changes in naming

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ function App() {
     <>
       <Header />
       <Routes>
+        
         <Route path="/" element={<Home />} />
         <Route path="/blog" element={<Blog />} />
         <Route path="/works" element={<WorksBig />} />
